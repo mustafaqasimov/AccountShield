64c8506fd1c6d63d99179e27f6f91cdfca415891
mustafaqasimov <mustafaqasimov2413@gmail.com>
2026-07-06 11:49:07 +0400

    Initial commit - AccountShield first commit

---
+++ api/src/test/java/com/accountshield/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountshield.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
